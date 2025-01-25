0b88f1a2340eedc26893012cf2ff8924e9591a0a
balddrAdmin <boliklevik@gmail.com>
2025-01-25 15:21:06 +0100

    7 lesson

--- users/views.py
+++ users/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
-def email_verification(request):
-    return render(request, 'users/email_verification.html')
+from users.forms import UserLoginForm, UserRegisterForm
+
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        print(form.errors)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegisterForm()
+    content = {
+        'form': form
+    }
+    return render(request, 'users/registration.html', content)
 
 def profile(request):
     return render(request, 'users/profile.html')
+
+def email_verification(request):
+    return render(request, 'users/email_verification.html')
